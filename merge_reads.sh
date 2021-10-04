59748419dd839782820baaf4995cc25a739a5332
qpmnguyen <quangpmnguyen@gmail.com>
2021-10-04 15:07:26 -0400

    merge_reads to create merged data

--- merge_reads.sh
+++ merge_reads.sh
@@ -1,4 +1,7 @@
-for f in "raw/*.fastq.gz"
-do 
-    echo "Processing $f file.."
+#!/usr/bin/env bash
+for f in raw/*.1.fastq.gz
+do
+    FILENAME=$(basename $f | awk '{split ($0, a, ".1.fastq.gz"); print a[1]}')
+    echo "Currently at $FILENAME"
+    cat "raw/$FILENAME.1.fastq.gz" "raw/$FILENAME.2.fastq.gz" > "combined/$FILENAME_combined.fastq.gz"
 done
